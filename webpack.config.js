6908db65499fcbe9d9cf29108d580fec003b6bab
jangrubin <wkdfnqls2465@gmail.com>
2023-06-02 14:54:35 +0900

    번들러 (webpack) 설정 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 // console.log(path.resolve(__dirname, 'dist'))
 module.exports = {
-  entry : "build/index.js",
+  entry : "./build/index.js",
   output : {
     path : path.resolve(__dirname, 'dist'),
     filename : "bundle.js"
   },
   // 특정 파일에 대한 변환, 처리 작업 수행
-  module : [ {
+  module :  {
+    rules : [{
     // 로더를 적용할 파일 형식
     test : /\.js$/,
     use : {
@@ -15,6 +16,9 @@ module.exports = {
       options : {
         presets : ['@babel/preset-env'],
       },
-    }
-  }],
+    },
+  }]},
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 }
