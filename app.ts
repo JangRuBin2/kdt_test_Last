ba2aeb3e4b5feafdd8119c997a85afb282a1b61f
jangrubin <wkdfnqls2465@gmail.com>
2023-06-02 14:58:45 +0900

    서버(express) 생성

--- app.ts
+++ app.ts
@@ -1,5 +1,18 @@
 import express from "express";
+// 있다가 root경로를 지정해주기 위한 path 모듈
+import path from "path";
 console.log(__dirname)
 const app = express();
-
-app.use(express.static())
+// 미들웨어 -> root
+app.use(express.static(__dirname));
+// 최초 페이지 불러오기
+app.get('/', (req, res)=> {
+  // res.sendFile() bundle파일 불러오면 됨
+})
+// 서버 실행
+app.listen(8080, (err : Error) => {
+  if(err) {
+    console.error("서버 실행에 실패했습니다",err);
+  }
+  console.log("서버가 실행 됐습니다.");
+})
